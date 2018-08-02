0e85ab2880e39c948abdad50cb97a0ec536c3936
Carlos Damázio <kaduardobsb95@gmail.com>
2018-08-02 02:19:44 -0300

    Added search, token and webhook mappings.

---
+++ tapioca_trello/resource_mapping/search.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+SEARCH_MAPPING = {
+    'search_retrieve_or_list': {
+        'resource': '/search',
+        'docs': (
+            'https://developers.trello.com/v1.0/reference
+            '#search-1'
+        ),
+        'methods': ['GET'],
+    },
+    'search_members_list': {
+        'resource': '/search/members',
+        'docs': (
+            'https://developers.trello.com/v1.0/reference'
+            '#searchmembers'
+        ),
+        'methods': ['GET'],
+    },
+}
